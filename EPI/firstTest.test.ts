e216c5cd922e8b13c09d0a2c0750834d0153d676
vincentole <63076449+vincentole@users.noreply.github.com>
2022-03-03 10:22:19 +0100

    added EPI

---
+++ EPI/firstTest.test.ts
@@ -0,0 +1,5 @@
+describe('is working', () => {
+    it('should work', () => {
+        expect(true).toBeTruthy();
+    });
+});
---
+++ jest.config.js
@@ -0,0 +1,14 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+    preset: 'ts-jest/presets/default-esm',
+    testEnvironment: 'node',
+    globals: {
+        'ts-jest': {
+            useESM: true,
+        },
+    },
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    roots: ['<rootDir>/ZTM Course/', '<rootDir>/EPI/'],
+};
